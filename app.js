a1253541ae67ed141f43e7cf6af555d8f76cf226
cony56 <taeyup56@hanmail.net>
2021-02-01 15:59:00 +0900

    'test'

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express();
 const port = process.env.PORT || 3000;
-
+//http://127.0.0.1:3000/
 app.get('/', (req,res) => res.send('Hello NodeJS!'));
 app.get('/welcome', (req,res) => res.send('Hi, there.Welcome to nodejs service'));
+app.get('/check', (req,res) => res.send('Example app listening on port 3000'));
 app.listen(port, () => console.log('Example app listening on port 3000'))
