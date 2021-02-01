850ebb24d93f73497ca68fdf309fe5646f4a010b
cony56 <taeyup56@hanmail.net>
2021-02-01 15:48:54 +0900

    added a new endpoint

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req,res) => res.send('Hello NodeJS!'))
+app.get('/', (req,res) => res.send('Hello NodeJS!'));
+app.get('/welcome', (req,res) => res.send('Hi, there.Welcome to nodejs service'));
 app.listen(port, () => console.log('Example app listening on port 3000'))
